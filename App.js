81d89957b8ac2ed31ef05fdac5d54d8fd95744af
Antoine Reveau <antoinerev@gmail.com>
2017-07-06 17:58:57 +0200

    first props

--- App.js
+++ App.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Text, View, Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+    );
+  }
+}
+
+export class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+
+export class HelloWorldApp extends Component {
   render() {
     return (
         <Text>Hello world!</Text>
@@ -10,4 +41,5 @@ export default class HelloWorldApp extends Component {
 }
 
 // skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => Bananas);
 AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
